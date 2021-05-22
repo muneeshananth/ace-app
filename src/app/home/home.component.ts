5d110a745538aaa3ea4baa2b898646279e169043
govimu8 <muneeshkumar.g@ptpll736.payoda.net>
2021-05-22 10:32:22 +0530

    alingment fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+// tslint:disable-next-line: only-arrow-functions
+declare function fatClientCheck(): any;
+declare function logo(): any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   config: any= {
     autoplay: 3000, // Autoplay option having value in milliseconds
     initialSlide: 3, // Slide Index Starting from 0
@@ -16,6 +19,9 @@ export class HomeComponent {
     prevButton: '.swiper-button-prev', // Class for prev button
     spaceBetween: 30 // Space between each Item
   };
-
+  public ngOnInit(): void {
+    fatClientCheck();
+    logo();
+  }
 
 }
