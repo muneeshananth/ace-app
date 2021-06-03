ddc114f23560419c6d06f90ccb224fa26e13cd17
govimu8 <muneeshkumar.g@ptpll736.payoda.net>
2021-06-03 11:16:05 +0530

    ogin changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { WizardComponent } from './wizard/wizard.component';
 import { StepFormSelectorComponent } from './wizard/step-form-selector/step-form-selector.component';
 import { FormStepDirective } from './directives/form-step.directive';
 import { MutiStepFormComponent } from './wizard/multi-step-form/multi-step-form.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -33,7 +34,7 @@ import { MutiStepFormComponent } from './wizard/multi-step-form/multi-step-form.
     FormsModule,
     RouterModule.forRoot([
       {
-        path: '', redirectTo: "/home", pathMatch: 'full'
+        path: '', redirectTo: "/login", pathMatch: 'full'
       },
       { path: 'home', component: HomeComponent },
       { path: 'products-detail', component: ProductDetailsComponent },
@@ -44,6 +45,7 @@ import { MutiStepFormComponent } from './wizard/multi-step-form/multi-step-form.
       { path: 'shipping', component: ShippingComponent },
       { path: 'cart3', component: CartTrackingComponent },
       { path: 'cart4', component: ProductAlertsComponent },
+      { path: 'login', component: LoginComponent },
 
     ])
   ],
@@ -62,6 +64,7 @@ import { MutiStepFormComponent } from './wizard/multi-step-form/multi-step-form.
     CartInfoComponent,
     CartAddressComponent,
     WizardComponent,
+    LoginComponent,
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   // providers:[{provide: MatStepperIntl, useClass: MyIntl}],
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+    ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+}
