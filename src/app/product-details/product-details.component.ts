c78e617092a9a5eee5a8f4744d8f80b924b36511
govimu8 <muneeshkumar.g@ptpll736.payoda.net>
2021-06-05 21:00:06 +0530

    product detail carousel added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
 import { CartService } from '../cart.service';
+declare function logo(): any;
 
 @Component({
   selector: 'app-product-details',
@@ -18,12 +19,7 @@ export class ProductDetailsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    // First get the product id from the current route.
-    const routeParams = this.route.snapshot.paramMap;
-    const productIdFromRoute = Number(routeParams.get('productId'));
-
-    // Find the product that correspond with the id provided in route.
-    this.product = products.find(product => product.id === productIdFromRoute);
+    logo();
   }
 
   addToCart(product) {
